3607d3342ca3698bb4b17870a8a18d601abe7ebc
Nitha P S <nithaps2002@gmail.com>
2025-10-17 10:42:04 +0530

    Add files via upload

---
+++ FunctionParameter.java
@@ -0,0 +1,9 @@
+public class FunctionParameter {
+    static void addnum(int num1,int num2){
+    int sum=num1+num2;
+    System.out.println("Result: "+sum);
+}
+public static void main(String[] args) {
+    addnum(2,5);
+}
+}
