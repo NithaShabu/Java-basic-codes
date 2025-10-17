a5c3ed297c66774353b51a97135ea17914ef7013
Nitha P S <nithaps2002@gmail.com>
2025-10-17 10:47:57 +0530

    Add files via upload

---
+++ FunSquareNum.java
@@ -0,0 +1,9 @@
+public class FunSquareNum {
+    static void square(int n){
+        int squ=(n*n);
+        System.out.println("Square of "+n+" is "+squ);
+    }
+    public static void main(String[] args) {
+        square(5);
+    }
+}
