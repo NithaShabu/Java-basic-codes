71c4e1ccb670ab5e1590244b29ec7e32a307801b
Nitha P S <nithaps2002@gmail.com>
2025-10-17 11:05:47 +0530

    Add files via upload

---
+++ FunReturn.java
@@ -0,0 +1,9 @@
+class FunReturn {
+    static int addition(int a,int b,int c){
+        int sum=a+b+c;
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(addition(2,9,5));
+    }
+}
